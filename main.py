7033514ad28b059d702c0d4bae6937d2f068c92d
Jrt345 <JonTech543@protonmail.com>
2022-08-10 20:19:19 -0700

    Create new function to create a password with a length ranging from 1 to 255

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
+import random
+import string
+
+uppercase = string.ascii_uppercase
+lowercase = string.ascii_lowercase
+nums = string.digits
+symbols = "!@#$%^&*"
+
 print("PassBird - A Simple Password Generator")
+
+
+def create_password(length):
+    password = ""
+
+    try:
+        if int(length) <= 0:
+            return "Error: {} is less than one, try again".format(int(length))
+
+        if int(length) > 255:
+            return "Error: {} is greater than 255, try again".format(int(length))
+
+        for i in range(int(length)):
+            selection = random.randint(1, 4)
+
+            if selection == 1:
+                password = password + random.choice(list(uppercase))
+            if selection == 2:
+                password = password + random.choice(list(lowercase))
+            if selection == 3:
+                password = password + random.choice(list(nums))
+            if selection == 4:
+                password = password + random.choice(list(symbols))
+    except ValueError:
+        return "Error: '{}' is not a number, try again".format(length)
+
+    return password
+
+
+while True:
+    print(create_password(input()))
