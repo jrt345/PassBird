c4ddca0e2290d0ae2f105ca3b7cb71ec3bd28196
Jrt345 <JonTech543@protonmail.com>
2022-08-20 12:04:36 -0700

    Add click options to include uppercase/lowercase letters, numbers, and/or symbols

--- main.py
+++ main.py
@@ -9,7 +9,21 @@ nums = string.digits
 symbols = "!@#$%^&*"
 
 
-def create_password(length):
+def get_selected_options(options):
+    options_selection = []
+
+    if not any(options):
+        for i in range(4):
+            options_selection.append(i + 1)
+
+    for i in range(4):
+        if options[i]:
+            options_selection.append(i + 1)
+
+    return options_selection
+
+
+def create_password(length, options):
     password = ""
 
     try:
@@ -20,7 +34,7 @@ def create_password(length):
             return "Error: {} is greater than 255, try again".format(int(length))
 
         for i in range(int(length)):
-            selection = random.randint(1, 4)
+            selection = random.choice(get_selected_options(options))
 
             if selection == 1:
                 password = password + random.choice(list(uppercase))
@@ -38,8 +52,16 @@ def create_password(length):
 
 @click.command()
 @click.option("--length", "-l", type=int, help="Length of password (1-255)")
-def main(length):
-    print(create_password(length))
+@click.option("--count", "-c", type=int, default=1, show_default=True, help="Print number of passwords at once")
+@click.option("--include_uppercase", "-uc", is_flag=True, help="Include uppercase letters")
+@click.option("--include_lowercase", "-lc", is_flag=True, help="Include lowercase letters")
+@click.option("--include_numbers", "-num", is_flag=True, help="Include numbers")
+@click.option("--include_symbols", "-sym", is_flag=True, help="Include symbols letters")
+def main(length, count, include_uppercase, include_lowercase, include_numbers, include_symbols):
+    options = (include_uppercase, include_lowercase, include_numbers, include_symbols)
+
+    for i in range(count):
+        print(create_password(length, options))
 
 
 if __name__ == '__main__':
