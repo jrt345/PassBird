cc85bcdb6cf3638fc6b216e45af70af4d1dcc70f
Jrt345 <JonTech543@protonmail.com>
2022-08-28 08:36:12 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='passbird',
     version='0.0.1',
+    license="MIT",
+    description="Python Password Generator - Generate strong and secure passwords!",
+    author="jrt345",
+    url="https://github.com/jrt345/PassBird",
     packages=find_packages(),
     install_requires=[
         "click"
