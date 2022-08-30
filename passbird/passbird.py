e7c8c0586833f8faadb1edd01013d430ee3215b5
Jrt345 <JonTech543@protonmail.com>
2022-08-29 21:24:40 -0700

    Add IntRange to click length and count options in passbird.py

--- passbird/passbird.py
+++ passbird/passbird.py
@@ -26,33 +26,24 @@ def get_selected_options(options):
 def create_password(length, options):
     password = ""
 
-    try:
-        if int(length) <= 0:
-            return "Error: {} is less than one, try again".format(int(length))
+    for i in range(int(length)):
+        selection = random.choice(get_selected_options(options))
 
-        if int(length) > 255:
-            return "Error: {} is greater than 255, try again".format(int(length))
-
-        for i in range(int(length)):
-            selection = random.choice(get_selected_options(options))
-
-            if selection == 1:
-                password = password + random.choice(list(uppercase))
-            if selection == 2:
-                password = password + random.choice(list(lowercase))
-            if selection == 3:
-                password = password + random.choice(list(nums))
-            if selection == 4:
-                password = password + random.choice(list(symbols))
-    except ValueError:
-        return "Error: '{}' is not a number, try again".format(length)
+        if selection == 1:
+            password = password + random.choice(list(uppercase))
+        if selection == 2:
+            password = password + random.choice(list(lowercase))
+        if selection == 3:
+            password = password + random.choice(list(nums))
+        if selection == 4:
+            password = password + random.choice(list(symbols))
 
     return password
 
 
 @click.command()
-@click.option("--length", "-l", type=int, help="Length of password (1-255)")
-@click.option("--count", "-c", type=int, default=1, show_default=True, help="Print number of passwords at once")
+@click.option("--length", "-l", type=click.IntRange(1, 255), help="Length of password")
+@click.option("--count", "-c", type=click.IntRange(1), default=1, show_default=True, help="Print number of passwords at once")
 @click.option("--include_uppercase", "-uc", is_flag=True, help="Include uppercase letters")
 @click.option("--include_lowercase", "-lc", is_flag=True, help="Include lowercase letters")
 @click.option("--include_numbers", "-num", is_flag=True, help="Include numbers")
