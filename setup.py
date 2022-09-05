865a6084f69624d766fdf7e81c3312e09d29f839
Jrt345 <JonTech543@protonmail.com>
2022-09-05 09:19:46 -0700

    Update setup.py to include more details about PassBird

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='passbird',
     version='0.0.1',
     license="MIT",
     description="Python Password Generator - Generate strong and secure passwords!",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="jrt345",
     url="https://github.com/jrt345/PassBird",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "click"
     ],
@@ -15,5 +21,12 @@ setup(
         'console_scripts': [
             'passbird = passbird.passbird:main'
         ]
-    }
+    },
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
 )
